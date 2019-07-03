27d5bc5344764d628df2f4a82ca78e7832a4e440
LuvJM <51397417+LuvJM@users.noreply.github.com>
2019-07-03 14:23:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def callback():
         sendText(user,'เขิลเลยยย')
     else :
         sendText(user,'ขอโทษนะเราไม่เข้าใจ.)
-              return '',200
+    return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
